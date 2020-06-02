ed2b68dc485ab1d20993541d1bb812cffdf167fe
Leonard Poissant <leonardtati@gmail.com>
2020-06-02 12:22:18 -0400

    fix the server

--- server/routes.js
+++ server/routes.js
@@ -1,38 +1,7 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const {viewerHandler ,commentHandler, individualPostHandler, exploreHandler, deleteWardrobeHandler, wardrobeHandler, unfollowHandler, deleteLookbookHandler, lookbookHandler, followHandler, changeAvatarHandler, signinHandler, signupHandler, postHandler, readHandler, userFetchHandler } = require('./handlers')
+const { testEndpoint } = require("./handlers");
 
-router.post('/api/signup', signupHandler)
-
-router.post('/api/post', postHandler)
-
-router.post('/api/signin', signinHandler)
-
-router.get('/api/post/:postId', individualPostHandler)
-
-router.get('/api/viewer/:type/:albumId/:userId', viewerHandler)
-
-router.get('/api/explore', exploreHandler)
-
-router.get('/api/read/:_id', readHandler)
-
-router.get('/api/user/:_id', userFetchHandler)
-
-router.put('/api/newcomment', commentHandler)
-
-router.put('/api/newlookbook', lookbookHandler)
-
-router.put('/api/deletelookbook', deleteLookbookHandler)
-
-router.put('/api/user/follow/:_id/:userId/', followHandler)
-
-router.put('/api/user/unfollow/:_id/:userId/', unfollowHandler)
-
-router.put('/api/user/avatar', changeAvatarHandler)
-
-router.put('/api/newwardrobe', wardrobeHandler)
-
-router.put('/api/deletewardrobe', deleteWardrobeHandler)
+router.post("/endpoint", testEndpoint);
 
 module.exports = router;
-
--- server/server.js
+++ server/server.js
@@ -1,15 +1,33 @@
-const express = require('express');
-const morgan = require('morgan');
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const request = require("request");
+
+require("dotenv").config();
 
 const PORT = 5678;
 
 var app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(require('./routes'));
+app.use(morgan("dev"));
+app.use(require("./routes"));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/", express.static(__dirname + "/"));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "OPTIONS, HEAD, GET, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const server = app.listen(PORT, function () {
-  console.info('🌍 Listening on port ' + server.address().port);
+  console.info("🌍 Listening on port " + server.address().port);
 });
