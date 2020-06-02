1fd04d79b9ebc2fdb849cdf27949a1a71a3a74c4
Kendra Scappaticci <k.scapp@gmail.com>
2020-06-02 14:56:45 -0400

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import styled from "styled-components";
+import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
 import "./App.css";
+
+import Kendra from "./components/Kendra/Kendra";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavWrapper>
+        <NavLink to="/">MoDA Home</NavLink>
+        <NavLink to="/kendra">Kendra</NavLink>
+      </NavWrapper>
       <Switch>
         <Route exact path="/">
           <div>MoDA</div>
+        </Route>
+        <Route exact path="/kendra">
+          <Kendra />
         </Route>
       </Switch>
     </BrowserRouter>
   );
 }
+
+const NavWrapper = styled.nav`
+  background: pink;
+  color: white;
+  position: relative;
+  font-size: 20px;
+  margin: 0;
+  padding: 10px 100px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
 
 export default App;
---
+++ src/components/Kendra/Kendra.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Kendra() {
+  return <div>KENDRA PAGE</div>;
+}
+
+export default Kendra;
